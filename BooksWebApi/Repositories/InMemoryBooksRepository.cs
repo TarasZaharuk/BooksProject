d94a31f3dbac80efb0519b7389627dff1fa93be7
Ivan Zaharuk <zaharuck@outlook.com>
2024-02-24 11:43:07 +0200

    Add backend project draft structure.

---
+++ BooksWebApi/Repositories/InMemoryBooksRepository.cs
@@ -0,0 +1,13 @@
+﻿using BooksProject.Shared;
+using BooksWebApi.Abstractions;
+
+namespace BooksWebApi.Repositories
+{
+    public class InMemoryBooksRepository : IBooksRepository
+    {
+        public BookDetailsDto GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
